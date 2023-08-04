b888e8bccac447c5436d65f891a3ebc8be028986
memosk8 <memolopez9363@gmail.com>
2023-08-04 15:28:07 -0600

    refactored

--- readFile.js
+++ readFile.js
@@ -15,17 +15,9 @@ import xlsx from "xlsx";
  * 
  */
 function readFile(path) {
-
   const file = xlsx.readFile(path);
-
-  var registrations = [];
-
-  const jsonSheet = xlsx.utils.sheet_to_json(file.Sheets[file.SheetNames[0]]);
-
-  jsonSheet.forEach((row) => registrations.push(row));
-
+  const registrations = xlsx.utils.sheet_to_json(file.Sheets[file.SheetNames[0]]);
   return registrations;
-
 }
 
 export default readFile;
