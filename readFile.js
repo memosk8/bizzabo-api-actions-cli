d010dfea23c6b0c07feda797d8cf441bb7c204ea
memosk8 <memolopez9363@gmail.com>
2023-08-04 10:54:31 -0600

    readfile now takes the path as a function

--- readFile.js
+++ readFile.js
@@ -1,20 +1,17 @@
 import xlsx from "xlsx";
-import os from 'node:os'
 
-const file = xlsx.readFile(
-  os.platform() === 'darwin' ? 
-    '/Users/guillermolopez/Downloads/tickets-461236.xlsx' : 
-    '/home/memosk8/Descargas/my_tickets.xlsx'
-)
+function readFile(path) {
 
-var registrations = []
+  const file = xlsx.readFile(path);
 
-const jsonSheet = xlsx.utils.sheet_to_json(file.Sheets[file.SheetNames[0]]);
+  var registrations = [];
 
-jsonSheet.forEach((row) => {
-  registrations.push(row)
-})
+  const jsonSheet = xlsx.utils.sheet_to_json(file.Sheets[file.SheetNames[0]]);
 
-console.log(registrations)
+  jsonSheet.forEach((row) => registrations.push(row));
 
-export default registrations;
+  return registrations;
+
+}
+
+export default readFile;
