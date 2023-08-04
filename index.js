c12fef5d9fa335ec16dfc6c56ba1910248f5069b
memosk8 <memolopez9363@gmail.com>
2023-08-04 15:27:47 -0600

    taking token from std input

--- index.js
+++ index.js
@@ -2,30 +2,37 @@ import * as readline from 'readline';
 import readFile from "./readFile.js"
 import updateReg from './UpdateReg.js';
 
+/* Timer helper to wait in the terminal */
 const timer = ms => new Promise(res => setTimeout(res, ms));
+
+let responses;
 
 const cli = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
 
+/* main menu */
+
 console.log(" 1 Bulk Update Registrations")
 console.log(" 2 Bulk Update Contacts")
 console.log(" 0 exit")
 
-cli.question('Select an option: \t', opt => {
+cli.question('Select an option:  ', opt => {
   switch (opt) {
     case '0':
       console.log("\nExiting...\n");
-      timer(1000).then(() => { 
-        console.log("done"); cli.close() 
+      timer(1000).then(() => {
+        console.log("done"); cli.close()
       });
       break;
 
     case '1':
       cli.question("File path: ", (path) => {
-        updateReg(readFile(path))
-        cli.close()
+        cli.question("API Token: ", (tk) => {
+          updateReg(readFile(path), tk)
+          cli.close()
+        })
       })
       break;
 
@@ -35,7 +42,7 @@ cli.question('Select an option: \t', opt => {
       break;
 
     default:
-      console.log("Please choose a valid option...");
+      console.log("Please choose a valid option...")
       cli.close()
       break;
   }
