1f541de95a6bc27b40fba99e2c66428099e86949
memosk8 <memolopez9363@gmail.com>
2023-08-04 10:54:06 -0600

    refactored code

--- index.js
+++ index.js
@@ -1,40 +1,37 @@
-import fetch from "node-fetch"
-import registrations from "./readFile.js"
-import 'dotenv/config.js'
+import * as readline from 'readline';
+import readFile from "./readFile.js"
+import updateReg from './UpdateReg.js';
 
-registrations.forEach(async (reg) => {
+// const timer = ms => new Promise(res => setTimeout(res, ms));
+// timer().then(_ => {console.log("done"); cli.close()});
 
-  let ticketId = reg["Ticket Number"]
-  let url = `https://api.bizzabo.com/api/registrations/${ticketId}`
-
-  const options = {
-    'method': 'PUT',
-    'headers': {
-      'Authorization': process.env.API_TOKEN,
-      'Content-Type': 'application/vnd.bizzabo.v2.0+json',
-      'Accept': 'application/json'
-    },
-    'body': JSON.stringify({ 
-  //  json body has to be stringified, if not the api will return an internal error 500
-      'properties': {
-        'firstName': reg['First Name'],
-        'lastName': reg['Last Name'],
-        'email': reg['Email Address'],
-        'checkedin': reg['Event Checked In']
-      }
-    }),
-    'checkedIn': reg["Event Checked In"],
-  }
-
-  console.log(JSON.stringify(options))
-
-  try {
-    const response = await fetch(url, options)
-    const data = await response.json()
-    console.log(data)
-  } catch (error) {
-    console.error(error);
-  }
-
+const cli = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
 });
 
+console.log(" 1 Bulk Update Registrations")
+console.log(" 2 Bulk Update Contacts")
+console.log(" 0 exit")
+
+cli.question('Select an option: \t', opt => {
+  switch (opt) {
+    case '0':
+      console.clear();
+      console.log("Exiting...");
+      cli.close();
+      break;
+
+    case '1':
+      cli.question("File path: ", (path) => {
+        updateReg(readFile(path))
+        cli.close()
+      })
+      break;
+      
+
+    default:
+      break;
+  }
+
+}); 
