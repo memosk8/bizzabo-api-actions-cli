6b1c8098dca39991fd6e52dcb7c0cc2e56cfc1d0
memosk8 <memolopez9363@gmail.com>
2023-08-05 11:04:15 -0600

    added upload contacts module

---
+++ UploadContacts.js
@@ -0,0 +1,43 @@
+import fetch from "node-fetch"
+import sleep from "sleep-promise";
+
+export default async function uploadContacts(contacts, token, eventID) {
+
+  for (let i = 0; i < contacts.length; i++) {
+
+    const contact = contacts[i];
+
+    const url = `https://api.bizzabo.com/api/contacts/?eventId=${eventID}`
+
+    const options = {
+      'method': 'POST',
+      'headers': {
+        'Authorization': `Bearer ${token}`,
+        'Content-Type': 'application/vnd.bizzabo.v2.0+json',
+        'Accept': 'application/json'
+      },
+      /*  body has to be stringified, if not 
+          the api will return an internal error 500  */
+      'body': JSON.stringify({
+        'properties': {
+          'email': contact['Email Address'],
+          'firstName': contact['First Name'],
+          'lastName': contact['Last Name'],
+          'company': contact['Company'],
+          'title': contact['Job Title']
+        }
+      })
+    }
+    // Async API call
+    try {
+      const res = await fetch(url, options)
+      const body = await res.json()
+      console.log(res.status, res.url, body.modified)
+    }
+    catch (error) {
+      console.error(error)
+    }
+    // to avoid the API call limit per second 
+    sleep(100)
+  }
+}
