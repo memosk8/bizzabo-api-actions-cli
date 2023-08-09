1889f649bf9b62c1407c6119dcf0382d0b850839
memosk8 <memolopez9363@gmail.com>
2023-08-09 13:53:06 -0600

    using prompt-sync for input and chalk for colors

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import readFile from "./readFile.js"
 import updateReg from './UpdateReg.js'
 import uploadContacts from './UploadContacts.js'
 import PromptSync from 'prompt-sync'
+import chalk from "chalk"
 
 const prompt = PromptSync({ sigint: true })
 let opt = ''
@@ -10,39 +11,44 @@ let opt = ''
 
 do {
   console.clear()
-  console.log(" 1 Bulk Update Registrations")
-  console.log(" 2 Bulk Upload Contacts")
-  console.log(" 0 exit")
+  console.log(chalk.yellow('1 - '), chalk.yellow.bgBlack.italic("Bulk Update Registrations"))
+  console.log(chalk.yellow('2 - '), chalk.yellow.bgBlack.italic("Bulk Upload Contacts"))
+  console.log(chalk.yellow('0 - '), chalk.redBright.bgBlack.italic("Exit\n"))
 
-  opt = prompt('\nSelect and option:  ')
+  opt = prompt(chalk.green.bgBlack('Select and option: '))
 
   switch (opt) {
 
     case '0':
-      console.log("\nExiting...\n")
+      console.log(chalk.redBright("\nExiting...\n"))
       process.exit(1)
 
     case '1':
-      const path = prompt('File path: ')
-      const tk = prompt('API token: ')
-      const registrations = readFile(path)
-      await updateReg(registrations, tk)
-      console.log('\n Bulk Update Registrations complete \n')
+      console.clear()
+      console.log(chalk.yellow.bold.bgBlack('\tBulk Update Registration\n'))
+      var path = prompt(chalk.green('File path: '))
+      var tk = prompt(chalk.green('API token: '))
+      var registrations = readFile(path)
+      var ok = await updateReg(registrations, tk)
+      if(!ok) {
+        console.error(chalk.red.bgBlack('An error ocurred, please check your input'))
+        process.exit(1)
+      }
+      console.log(chalk.green('\n============='))
+      console.log(chalk.blue.bold('Bulk Update Registrations complete'))
+      console.log(chalk.green('=============\n'))
       process.exit(1)
 
     case '2':
-      cli.question("File path: ", (path) => {
-        cli.question("API Token: ", (tk) => {
-          cli.question("Event ID: ", (eventID) => {
-            uploadContacts(readFile(path), tk, eventID)
-            cli.close()
-          })
-        })
-      })
-      break
+      var path = prompt('File path: ')
+      var tk = prompt('API token: ')
+      var eventID = prompt('Event #ID: ')
+      var contacts = readFile(path)
+      await uploadContacts(contacts, tk, eventID)
+      console.log('\n==============\n Bulk Upload Contacts complete \n==============\n')
+      process.exit(1)
 
     default:
-      console.log('< Please select a valid option >')
       break
   };
 
