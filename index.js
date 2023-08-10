7c8ed1645f35f83f787c01ec2fcad50deafb93e4
Guillermo López <78505499+memosk8@users.noreply.github.com>
2023-08-09 20:35:12 -0600

    update index.js

--- index.js
+++ index.js
@@ -38,9 +38,9 @@ do {
       console.log()
       await updateReg(registrations, tk)
       await sleep(300)
-      console.log(chalk.green('\n\t======================================'))
-      console.log(chalk.green('\t|'), chalk.rgb(250, 189, 5).bold('Bulk Update Registrations complete'), chalk.green('|'))
-      console.log(chalk.green('\t======================================\n'))
+      console.log(chalk.green('\n======================================'))
+      console.log(chalk.green('|'), chalk.rgb(250, 189, 5).bold('Bulk Update Registrations complete'), chalk.green('|'))
+      console.log(chalk.green('======================================\n'))
       process.exit(1)
 
 
