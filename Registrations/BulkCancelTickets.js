3f4de18c391d3d30a6e4a7c4b9acff060d081e7f
memosk8 <memolopez9363@gmail.com>
2023-09-16 11:55:30 -0600

    renamed registrations folder

---
+++ Registrations/BulkCancelTickets.js
@@ -0,0 +1,53 @@
+import fetch from "node-fetch"
+//import sleep from "./sleep.js"
+import chalk from "chalk";
+
+/**
+ * @param {String} eventId 
+ * The event ID #
+ * 
+ * @param {Array<Object>} registrations 
+ * The registrations array of objects
+ * 
+ * @param {String} token 
+ * The API token for the account
+ * 
+ * @description
+ * Creates the URL to call based upon each ticket number, generates
+ * the headers and body of the request, and makes the call to the API.
+ * Logs the endpoints, status and 
+ * 
+ * @returns
+ * BAD_TOKEN if the provided token is invalid - Error 401
+ *  or true if ok
+ */
+
+export default async function BulkCancelTickets(eventId, registrations, token) {
+
+  for (let i = 0; i < registrations.length; i++) {
+
+    const reg = registrations[i];
+    const url = `https://api.bizzabo.com/api/registrations/cancel?eventId=${eventId}&orderId=${reg['Order Number']}&ticketId=${reg['Ticket Number']}&validity=invalid&refundAmount=${0}&sendEmail=false`
+    const options = {
+      'method': 'PUT',
+      'headers': {
+        'Authorization': `Bearer ${token}`,
+        'Content-Type': 'application/vnd.bizzabo.v2.0+json',
+        'Accept': 'application/json'
+      },
+    }
+
+    try {
+      const res = await fetch(url, options)
+      if (res.status === 401) {
+        return 'BAD_TOKEN'
+      }
+      console.log(chalk.yellow(res.status))
+      console.log(chalk.red(`Cancelled Ticket #${reg['Ticket Number']}`))
+    }
+    catch (error) {
+      console.error(error)
+    }
+  }
+  return true;
+}
---
+++ Registrations/GetMagicLinks.js
@@ -0,0 +1,67 @@
+import fetch from "node-fetch"
+
+/**
+ * 
+ * @description
+ * Get all registranst magic links into a spreadsheet 
+ * 
+ * @param {String} token 
+ * The API token 
+ * 
+ * @param {String} eventId 
+ * The event ID to get registrations
+ * 
+ * @returns
+ * BAD_TOKEN if the provided token is invalid (Error 401)
+ *  or true if ok
+ */
+
+export default async function GetMagicLinks(token, eventId) {
+
+  const url = `https://api.bizzabo.com/api/registrations/?eventId=${eventId}`
+  const options = {
+    'method': 'GET',
+    'headers': {
+      'Authorization': `Bearer ${token.trim()}`,
+      'Content-Type': 'application/vnd.bizzabo.v2.0+json',
+      'Accept': 'application/json'
+    }
+  }
+
+  try {
+    const res = await fetch(url, options)
+
+    if (res.status === 400) return { 
+      status: false, 
+      error: "NOT_FOUND", 
+      message: "Please check the #event ID " 
+    }
+    else if (res.status === 401) return { 
+      status: false, 
+      error: 'BAD_TOKEN', 
+      message: "Please check your API token" 
+    }
+    else {
+      let magicLinks = []
+      const body = await res.json()
+
+      for (let i = 0; i < body.content.length; i++) {
+        const registration = body.content[i];
+
+        magicLinks.push({
+          'First Name': registration.properties.firstName,
+          'Last Name': registration.properties.lastName,
+          'Email': registration.properties.email,
+          'Ticket Number': registration.id,
+          'Ticket Name': registration.ticketName,
+          'Magic Link': registration.magicLink
+        })
+      }
+
+      return magicLinks;
+    }
+  }
+  catch (error) {
+    console.error(error)
+  }
+}
