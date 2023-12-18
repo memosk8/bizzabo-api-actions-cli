e7bbb69848e36d076698f6532a7215fcab92b21d
memosk8 <memolopez9363@gmail.com>
2023-12-18 15:00:11 -0600

    getAuth will write to a file the token and read from it to validate the expiration date

--- public/getAuth.js
+++ public/getAuth.js
@@ -1,13 +1,39 @@
 import { AuthenticationBuilder } from 'bizzabo-api';
+import * as fs from 'fs'
 
 export default async function getAuth(id, secret, acc) {
 
-  const
-    CLIENT_ID = id,
-    CLIENT_SECRET = secret,
-    ACCOUNT_ID = acc,
-    auth = new AuthenticationBuilder(CLIENT_ID, CLIENT_SECRET, ACCOUNT_ID).build(),
+  let token = fs.readFileSync('public/tk.json', { encoding: 'utf8', flag: 'r' })
+
+  // if there is no token in the file
+  // create a new one 
+  if (token == "") {
+
+    const
+      CLIENT_ID = 'BirLN0YIR9QD8grWZ9TGHLLceC6tPtay',
+      CLIENT_SECRET = '1FjXvLzkjW_tVS8Ysy33C5SsIF-uH3Zy8_w12PsCQ39ciM5Z_lR-g4JQm8gFG8ih',
+      ACCOUNT_ID = 192668,
+      auth = new AuthenticationBuilder(CLIENT_ID, CLIENT_SECRET, ACCOUNT_ID).build();
+
     token = await auth.getClientCredentialsToken()
+
+    // save new token to file ti
+    const newToken = {
+      date: new Date(),
+      ...token
+    }
+    fs.writeFileSync('public/tk.json', JSON.stringify(newToken), { encoding: 'utf8' })
+  }
+
+  else if (token != "") {
+    let tk = JSON.parse(token)
+    let aDay = new Date().getTime() + (24*60*60*1000)
+    if(tk.date >= aDay){
+      // @TODO refactor code to generate token 
+    }
+  }
 
   return token;
 }
+
+getAuth()
